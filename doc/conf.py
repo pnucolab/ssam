b5e7a37ba088f961db867d33b6449cdc45e9eb4e
Jeongbin Park <pjb7687@gmail.com>
2024-10-12 23:11:08 +0900

    update according to rtd v2

--- doc/conf.py
+++ doc/conf.py
@@ -177,6 +177,6 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 autodoc_mock_imports = ["ssam.utils"]
